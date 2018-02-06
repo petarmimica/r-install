646e435c6e272dde57a7099f50fd0fca99ba92f4
Petar Mimica <pere@magnetization.daa.uv.es>
2018-02-06 14:45:13 +0100

    Added "data.table".

--- install.R
+++ install.R
@@ -1,9 +1,10 @@
 # This is the package reinstaller for when R and/or R-Studio is updated.
 
-packages <- c("ggplot2", "dplyr", "tidyr", "gridExtra", "Rcpp", "doParallel", "rmarkdown", "hashmap", "ggfortify", "ggrepel", "treemapify", "ggforce", "ggridges", "broom", "purrr", "spatialfil", "microbenchmark", "devtools")
+packages <- c("ggplot2", "dplyr", "tidyr", "gridExtra", "Rcpp", "doParallel", "rmarkdown", "hashmap", "ggfortify", "ggrepel", "treemapify", "ggforce", "ggridges", "broom", "purrr", "spatialfil", "microbenchmark", "devtools", "data.table")
 
 install.packages(packages)
 
 # install HDF
 source("https://bioconductor.org/biocLite.R")
 biocLite("rhdf5")
+
