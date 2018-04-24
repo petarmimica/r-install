64e4109c7c75ef1e80f75d2934249f8537a63598
Petar Mimica <pere@magnetization.daa.uv.es>
2018-04-24 09:14:48 +0200

    Added caret and h2o to the list.

--- install.R
+++ install.R
@@ -1,10 +1,9 @@
 # This is the package reinstaller for when R and/or R-Studio is updated.
 
-packages <- c("ggplot2", "dplyr", "tidyr", "gridExtra", "Rcpp", "doParallel", "rmarkdown", "hashmap", "ggfortify", "ggrepel", "treemapify", "ggforce", "ggridges", "broom", "purrr", "spatialfil", "microbenchmark", "devtools", "data.table")
+packages <- c("ggplot2", "dplyr", "tidyr", "gridExtra", "Rcpp", "doParallel", "rmarkdown", "hashmap", "ggfortify", "ggrepel", "treemapify", "ggforce", "ggridges", "broom", "purrr", "spatialfil", "microbenchmark", "devtools", "data.table", "caret", "h2oa")
 
 install.packages(packages)
 
 # install HDF
 source("https://bioconductor.org/biocLite.R")
 biocLite("rhdf5")
-
